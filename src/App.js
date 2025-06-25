2be2f46d8fe6eef6ccf5face7d7327b1c8034723
Harshit <pharshit2003@gmail.com>
2025-06-25 10:40:52 +0530

    added bottom sheet basic layout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BottomSheet from './components/BottomSheet/BottomSheet';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="main-heading">My React App</h1>
+      <BottomSheet />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BottomSheet/BottomSheet.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BottomSheet.css';
+
+export default function BottomSheet() {
+  return (
+    <div className="bottom-sheet">
+      <div className="handle" />
+      <div className="content">
+        <h2>Bottom Sheet</h2>
+        <p> bottom sheet layout</p>
+      </div>
+    </div>
+  );
+}
